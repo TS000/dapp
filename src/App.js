05d46e68b3d2316b47da6dae5e93cfd67e234908
TS000 <tylersnell@me.com>
2021-12-13 19:37:38 -0800

    set up requests

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import './App.css';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+const App = () => {
+  const [greeting, setGreetingValue] = useState()
+
+  const requestAccount = async () => {
+    await window.ethereum.request({ method: 'eth_request' })
+  }
+
+  const fetchGreeting = async () => {
+    if ( typeof window.ethereum !== 'undefined' ) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+      try {
+        const data = await contract.greet()
+        console.log('data: ', data)
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+      }
+    }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
